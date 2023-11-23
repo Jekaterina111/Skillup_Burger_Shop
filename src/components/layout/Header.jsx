04859dd62ca875d700ff28c13f36e8e10b043ab9
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-11-23 19:45:52 +0200

    add humburger menu in main paidge

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiShoppingCart } from 'react-icons/gi';
+import { TfiMenu } from 'react-icons/tfi';
 import { IoFastFoodSharp } from 'react-icons/io5';
 import DropdownMenu from './DropdownMenu';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
       <nav>
         <div className="icon-container">
           <IoFastFoodSharp />
         </div>
-        <div>
+        <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
           <Link to="/">Home</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/about">About</Link>
@@ -21,6 +28,9 @@ const Header = () => {
             </div>
           </Link>
           <DropdownMenu />
+        </div>
+        <div className="hamburger-menu icon-container" aria-hidden="true" onClick={toggleMenu}>
+          <TfiMenu />
         </div>
       </nav>
     </header>
